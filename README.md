ca9fd6245319d560fdfd568a788b65228f6eef72
Prasanna Gautam <prasannagautam@gmail.com>
2015-07-05 20:34:39 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Kodi CLI 
+
+For navigating from commandline. This is a very simple application written so I didn't have to reach to a remote control while watching tv. Might add a few things as needed.
+You'll have to install the JSON-RPC service from settings in Kodi.
+
+
+# How to Run
+
+## Change the config to suit your setup
+
+Save the following with appropriate changes as `config.ini`. 
+
+```
+[Kodi]
+server=192.168.1.60
+port=8080
+```
+
+## Install requirements
+
+Only "requests" library is used other than curses. In case I'll add more things, follow the instructions.
+
+`pip install -r requirements.txt `
+
+## Run the application
+
+`python kodi.cli`
+
+### ???
+
+### Profit
---
+++ kodi-cli.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+# See: http://www.tuxradar.com/content/code-project-build-ncurses-ui-python
+from os import system
+import curses
+import requests
+import json
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'config.ini'))
+base_url= config.get('Kodi', 'server')
+port = config.get('Kodi', 'port')
+url = "http://{}:{}/jsonrpc".format(base_url,port)
+
+headers = {'content-type': 'application/json'}
+
+
+
+def send_input_command(cmd):
+  payload = json.dumps({"jsonrpc": "2.0", 
+                        "method": cmd})
+  return requests.request("POST", url, data=payload, headers=headers)
+
+
+send_input_command("Input.Down")
+
+def get_param(prompt_string):
+     screen.clear()
+     screen.border(0)
+     screen.addstr(2, 2, prompt_string)
+     screen.refresh()
+     input = screen.getstr(10, 10, 60)
+     return input
+
+
+x = 0
+
+while x != ord('q'):
+     screen = curses.initscr()
+     curses.echo()  
+     screen.clear()
+     screen.border(0)
+     screen.addstr(2, 2, "Move... (Arrow keys also work for navigation)")
+     screen.addstr(4, 4, "w: Up")
+     screen.addstr(5, 4, "a: Down")
+     screen.addstr(6, 4, "s: Left")
+     screen.addstr(7, 4, "d: Right")
+     screen.addstr(8, 4, "g: Select")
+     screen.addstr(9, 4, "b: Back")
+     screen.addstr(10, 4, "h: Home")
+     screen.addstr(11, 4, "q: Exit")
+
+     screen.refresh()
+     
+     x = screen.getch()
+     screen.addstr(12, 5, str(x))
+
+     if x == ord('w') or x == 65:
+      send_input_command("Input.Up")
+
+
+     if x == ord('a') or x == 68:
+      send_input_command("Input.Left")
+ 
+     if x == ord('s') or x == 66:
+      send_input_command("Input.Down")
+
+     if x == ord('d') or x == 67:
+       send_input_command("Input.Right")
+
+     if x == ord('g'):
+       send_input_command("Input.Select")
+     
+     if x == ord('h'):
+       send_input_command("Input.Home")
+
+     if x == ord('b'):
+       send_input_command("Input.Back")
+curses.endwin()
